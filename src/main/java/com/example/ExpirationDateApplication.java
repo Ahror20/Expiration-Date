e15e8f4a6aa482356bda32cb53e346dcbede8457
ahror <ahrororzimurodov@icloud.com>
2024-03-16 18:37:03 +0500

    first commit

---
+++ src/main/java/com/example/ExpirationDateApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class ExpirationDateApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(ExpirationDateApplication.class, args);
+
+    }
+
+}
